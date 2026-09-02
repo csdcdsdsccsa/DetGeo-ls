a5980fedea605704e0383bce2b468824dffabc4c
csdcdsdsccsa <210679589+csdcdsdsccsa@users.noreply.github.com>
2026-09-02 10:46:16 +0800

    fix: make frozen head experiment scripts runnable

--- scripts/run_test_droneaerial.sh
+++ scripts/run_test_droneaerial.sh
@@ -1,3 +1,4 @@
-python train.py --detector_head yolo --val --pretrain saved_models/model_droneaerial_yolo_bs8_model_best.pth.tar --emb_size 512 --img_size 1024 --data_root data --data_name CVOGL_DroneAerial --savename test_model_droneaerial_yolo --gpu 0 --batch_size 8 --num_workers 8 --print_freq 50
+PYTHON_BIN="${PYTHON_BIN:-python}"
+"$PYTHON_BIN" train.py --detector_head yolo --val --pretrain saved_models/frozen_model_droneaerial_yolo_bs8_model_best.pth.tar --emb_size 512 --img_size 1024 --data_root data --data_name CVOGL_DroneAerial --savename test_frozen_model_droneaerial_yolo --gpu 0 --batch_size 8 --num_workers 8 --print_freq 50
 
-python train.py --detector_head yolo --test --pretrain saved_models/model_droneaerial_yolo_bs8_model_best.pth.tar --emb_size 512 --img_size 1024 --data_root data --data_name CVOGL_DroneAerial --savename test_model_droneaerial_yolo --gpu 0 --batch_size 8 --num_workers 8 --print_freq 50
+"$PYTHON_BIN" train.py --detector_head yolo --test --pretrain saved_models/frozen_model_droneaerial_yolo_bs8_model_best.pth.tar --emb_size 512 --img_size 1024 --data_root data --data_name CVOGL_DroneAerial --savename test_frozen_model_droneaerial_yolo --gpu 0 --batch_size 8 --num_workers 8 --print_freq 50
